c4490f3717a51fa9a9c25050b90cb43dd740b14d
Nathan Brown <>
2023-11-28 15:02:15 -0700

    Use AsyncEventingBasicConsumer in server

--- GrpcGreeter/RabbitMqServer.cs
+++ GrpcGreeter/RabbitMqServer.cs
@@ -23,7 +23,8 @@ public class RabbitMqServer : IHostedService
 
     public Task StartAsync(CancellationToken cancellationToken = default)
     {
-        var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+        var factory = new ConnectionFactory
+            { HostName = "localhost", UserName = "guest", Password = "guest", DispatchConsumersAsync = true, };
         _connection = factory.CreateConnection();
 
         for (int i = 0; i < _parallelCount; i++)
@@ -42,8 +43,8 @@ public class RabbitMqServer : IHostedService
 
             channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += OnMessageReceived;
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += OnMessageReceivedAsync;
             channel.BasicConsume(queue: QUEUE_NAME,
                 autoAck: false,
                 consumer: consumer);
@@ -54,14 +55,14 @@ public class RabbitMqServer : IHostedService
         return Task.CompletedTask;
     }
 
-    private void OnMessageReceived(object? consumer, BasicDeliverEventArgs ea)
+    private async Task OnMessageReceivedAsync(object? consumer, BasicDeliverEventArgs ea)
     {
-        var channel = (consumer as EventingBasicConsumer)?.Model;
+        var channel = (consumer as IBasicConsumer)?.Model;
         if (channel == null || channel.IsClosed) throw new OperationCanceledException("Channel closed");
         var serviceShutdownToken = this._serviceCancellationTokenSource.Token;
 
         var call = new RabbitRpcRequestCall(channel, ea);
-        var _ = call.DoCall(_fibService.GetFibAsync, serviceShutdownToken);
+        await call.DoCall(_fibService.GetFibAsync, serviceShutdownToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
